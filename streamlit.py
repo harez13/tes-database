9c976b84d6ccb942354c142d3e028912dbc130ec
Hae Reza Putra Bhakti <haerezaputra6@gmail.com>
2025-07-16 12:30:19 +0700

    clear on submit

--- streamlit.py
+++ streamlit.py
@@ -10,7 +10,7 @@ init_db()
 
 st.title("📋 INPUT KINERJA ITSUPPORT")
 
-with st.form("data_form"):
+with st.form("data_form", clear_on_submit=True):
     name = st.text_input("Nama Lengkap")
     email = st.text_input("Email")
     umur = st.number_input("Umur", min_value=1, max_value=120)
@@ -21,6 +21,7 @@ with st.form("data_form"):
         if name and email:
             insert_data(name, email, umur, divisi)
             st.success("✅ Data berhasil disimpan!")
+
         else:
             st.warning("⚠️ Mohon lengkapi semua kolom!")
 
@@ -34,7 +35,7 @@ init_db2()
 
 st.title("📋 INPUT KINERJA ITSUPPORT2")
 
-with st.form("data_form2"):
+with st.form("data_form2", clear_on_submit=True):
     name = st.text_input("Nama Lengkap")
     email = st.text_input("Email")
     umur = st.number_input("Umur", min_value=1, max_value=120)
