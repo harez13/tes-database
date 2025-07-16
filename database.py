db113d21f27001a9843f382aa29e820aa7413ecb
Hae Reza Putra Bhakti <haerezaputra6@gmail.com>
2025-07-16 12:20:59 +0700

    edited fetchall func

--- database.py
+++ database.py
@@ -35,4 +35,41 @@ def fetch_all():
 
     return pd.DataFrame(data, columns=column)
     conn.close()
-    
+
+
+
+
+DB_NAME2 = "datapegawai2.db"
+
+def init_db2():
+    conn = sqlite3.connect(DB_NAME2)
+    c = conn.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS pegawai (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT,
+            umur INTEGER,
+            divisi TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+def insert_data2(name, email, umur, divisi):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute("INSERT INTO pegawai(name, email, umur, divisi) VALUES (?, ?, ?, ?)", (name, email, umur, divisi))
+    conn.commit()
+    conn.close()
+
+def fetch_all2():
+    with sqlite3.connect(DB_NAME) as conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM pegawai ORDER BY id DESC")
+        data = c.fetchall()
+        
+        column = [col[0] for col in c.description]
+
+    return pd.DataFrame(data, columns=column)
+    conn.close()
