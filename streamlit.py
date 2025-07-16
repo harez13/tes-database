41e63ea8b0fb7c3be2be2fd9028fe33b4c3b7ea5
Hae Reza Putra Bhakti <haerezaputra6@gmail.com>
2025-07-16 10:43:35 +0700

    edited streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import mysql.connector
 
 st.title('Test Database')
 
-conn = st.connection('mysql', type='sql')
-
-df = conn.query('select * from orders')
-
-st.dataframe(df)
+conn = mysql.connector.connect(
+    host = st.secrets['mysql']['host'],
+    port = st.secrets['mysql']['port'],
+    database = st.secrets['mysql']['database'],
+    user = st.secrets['mysql']['user'],
+    password = st.secrets['mysql']['password']
+)
